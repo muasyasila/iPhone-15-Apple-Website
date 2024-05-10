00b4eeb65453faf1a9893a7d22b920108d62175f
muasyasila <curtissila4@gmail.com>
2024-05-10 11:56:16 +0300

    Model view

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,7 +1,19 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap";
+import ModelView from "../components/ModelView"
+import { useState } from "react";
+import { yellowImg } from "../utils"
 
 const Model = () => {
+    const [size, setSize] = useState('small');
+    const [model, setModel] = useState({
+        title: 'iPhone 15 Pro in Natural Titanium',
+        color: ['#8F8A81', '#FFE7B9', '#6F6C64'],
+        img: yellowImg,
+    })
+
+    //Camera control for the model view
+
     useGSAP(() => {
     gsap.to('#heading', { y: 0, opacity: 1 } )
     }, []);
@@ -12,8 +24,15 @@ const Model = () => {
             <h1 id="heading" className="section-heading">
                 Take a closer look.
             </h1>
-        </div>
+            <div className="flex flex-col items-center mt-5">
+                <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+                    <ModelView />
+                </div>
 
+            </div>
+
+
+        </div>
     </section>
   )
 }
