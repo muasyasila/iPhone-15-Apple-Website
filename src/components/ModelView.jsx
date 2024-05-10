0dfb36d4fbafceb9cdd4791f0890a81d94f089f0
muasyasila <curtissila4@gmail.com>
2024-05-10 18:46:16 +0300

    iPhone 3D Model added

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { PerspectiveCamera, View } from "@react-three/drei"
 
-const ModelView = () => {
+import Lights from './Lights';
+import IPhone from './IPhone';
+import { Suspense } from "react";
+
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotatioSize, size, item}) => {
   return (
-    <div>ModelView</div>
+    <View 
+    index={index}
+    id={gsapType}
+    className={`border-2 border-red-500 w-full h-full {index == 2} ? 'right-[-100%] : ''`}
+    >
+   {/*Ambient Light*/}
+   <ambientLight intensity={0.3} />
+
+   <PerspectiveCamera  makeDefault position={[0, 0, 4]}/>
+    <Lights />
+    
+    <Suspense fallback={<div>Loading</div>}>
+      <IPhone />
+    </Suspense>
+    </View>
   )
 }
 
