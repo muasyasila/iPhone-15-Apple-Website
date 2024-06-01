a7dd2570eaa493db3ffbd91a803d180b18fa874f
muasyasila <curtissila4@gmail.com>
2024-06-01 11:39:02 +0300

    Product section update

--- src/components/CardCarousel.jsx
+++ src/components/CardCarousel.jsx
@@ -22,10 +22,12 @@ const CardCarousel = () => {
 
   return (
     <div className="container mx-auto p-4 bg-zinc">
+        
+        <h1 id="title" className="section-heading mb-8 mt-8 ml-4">The latest. Take a look at what else is new.</h1>
        <Slider {...settings}>
         {cards.map((card) => (
           <div key={card.id} className="p-4" style={{ width: 'calc(100% / 4)' }}>
-            <div className="relative bg-cover bg-center h-96 rounded-lg shadow-lg overflow-hidden" style={{ backgroundImage: `url(${card.image})` }}>
+            <div className="mt-8 relative bg-cover bg-center h-96 rounded-3xl shadow-lg overflow-hidden" style={{ backgroundImage: `url(${card.image})` }}>
               <div className="absolute top-0 left-0 bg-gradient-to-b from-black to-transparent w-full p-4">
                 <h2 className="text-white text-lg">{card.title}</h2>
                 <p className="text-white text-xl font-bold mt-2">{card.description}</p>
