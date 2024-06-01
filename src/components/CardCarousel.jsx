28fba6358462b44658300ea896c961236e63e44a
muasyasila <curtissila4@gmail.com>
2024-06-01 12:55:16 +0300

    Product section update

--- src/components/CardCarousel.jsx
+++ src/components/CardCarousel.jsx
@@ -13,19 +13,27 @@ const CardCarousel = () => {
   useEffect(() => {
     cardRefs.current.forEach((card, index) => {
       if (card) {
-        const hoverIn = () => gsap.to(card, { scale: 1.05, duration: 0.3, ease: 'power1.inOut' });
-        const hoverOut = () => gsap.to(card, { scale: 1, duration: 0.3, ease: 'power1.inOut' });
-        
-        card.addEventListener('mouseenter', hoverIn);
-        card.addEventListener('mouseleave', hoverOut);
-
-        // Cleanup function to remove event listeners
-        return () => {
-          card.removeEventListener('mouseenter', hoverIn);
-          card.removeEventListener('mouseleave', hoverOut);
-        };
+        card.addEventListener('mouseenter', () => {
+          gsap.to(card, { scale: 1.05, duration: 0.3, ease: 'power1.inOut' });
+        });
+        card.addEventListener('mouseleave', () => {
+          gsap.to(card, { scale: 1, duration: 0.3, ease: 'power1.inOut' });
+        });
       }
     });
+
+    return () => {
+      cardRefs.current.forEach((card) => {
+        if (card) {
+          card.removeEventListener('mouseenter', () => {
+            gsap.to(card, { scale: 1.05, duration: 0.3, ease: 'power1.inOut' });
+          });
+          card.removeEventListener('mouseleave', () => {
+            gsap.to(card, { scale: 1, duration: 0.3, ease: 'power1.inOut' });
+          });
+        }
+      });
+    };
   }, []);
 
   const settings = {
@@ -42,7 +50,7 @@ const CardCarousel = () => {
 
   return (
     <div className="container mx-auto p-4 bg-zinc">
-      <h1 id="carousel-title" className="section-heading mb-8 mt-8 ml-4">
+      <h1 id="title" className="section-heading mb-8 mt-8 ml-4">
         The latest. Take a look at what else is new.
       </h1>
       <Slider {...settings}>
@@ -50,7 +58,7 @@ const CardCarousel = () => {
           <div key={card.id} className="p-4" style={{ width: 'calc(100% / 4)' }}>
             <div
               ref={(el) => (cardRefs.current[index] = el)}
-              className="mt-8 relative bg-cover bg-center h-[500px] rounded-2xl shadow-lg overflow-hidden card"
+              className="mt-8 relative bg-cover bg-center h-[500px] rounded-2xl shadow-lg overflow-hidden"
               style={{ backgroundImage: `url(${card.image})` }}
             >
               <div className="absolute top-0 left-0 bg-gradient-to-b from-black to-transparent w-full p-4">
